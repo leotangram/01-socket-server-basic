d6fd06bbb224ca2181864568dc245c06c6ebbc96
Leonardo Omaña <leotangram@gmail.com>
2021-06-02 16:45:37 -0500

    Frontend minichat

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-const app = require('express')()
+const express = require('express')
+const app = express()
 const server = require('http').createServer(app)
 const io = require('socket.io')(server)
+app.use(express.static(`${__dirname}/public`))
 io.on('connection', () => {
-  /* … */
+  console.log('Client connection established')
 })
 server.listen(8080, () => {
   console.log('Server listening on port 8080')
