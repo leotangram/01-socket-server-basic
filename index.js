618e8d580e5f2c1205899cbb897ce3b71a8a54ce
Leonardo Omaña <leotangram@gmail.com>
2021-06-03 10:25:37 -0500

    Send and receive messages from & to server

--- index.js
+++ index.js
@@ -4,11 +4,14 @@ const server = require('http').createServer(app)
 const io = require('socket.io')(server)
 app.use(express.static(`${__dirname}/public`))
 io.on('connection', (socket) => {
-  socket.emit('welcome-message', {
-    message: 'Welcome to server',
-    date: new Date(),
+  // socket.emit('welcome-message', {
+  //   message: 'Welcome to server',
+  //   date: new Date(),
+  // })
+  socket.on('message-to-server', (data) => {
+    console.log(data)
+    io.emit('message-from-server', data)
   })
-  socket.on('client-message', (data) => console.log(data))
 })
 server.listen(8080, () => {
   console.log('Server listening on port 8080')
