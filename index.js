5dcd9c6a591fdfe6d60cf7f00c60eb747feb90bc
Leonardo Omaña <leotangram@gmail.com>
2021-06-03 12:01:44 -0500

    Config sockets in server class

--- index.js
+++ index.js
@@ -3,14 +3,3 @@ const Server = require('./models/server')
 const server = new Server()
 
 server.execute()
-
-// io.on('connection', (socket) => {
-//   // socket.emit('welcome-message', {
-//   //   message: 'Welcome to server',
-//   //   date: new Date(),
-//   // })
-//   socket.on('message-to-server', (data) => {
-//     console.log(data)
-//     io.emit('message-from-server', data)
-//   })
-// })
--- models/server.js
+++ models/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const http = require('http')
 const socketio = require('socket.io')
 const path = require('path')
+const Sockets = require('./sockets')
 
 class Server {
   constructor() {
@@ -16,7 +17,7 @@ class Server {
     this.app.use(express.static(path.resolve(__dirname, '../public')))
   }
   configureSockets() {
-    // ???
+    new Sockets(this.io)
   }
   execute() {
     this.middlewares()
