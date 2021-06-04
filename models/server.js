6ffe23c3928a560b1a08798682e7fa979c936daf
Leonardo Omaña <leotangram@gmail.com>
2021-06-04 10:26:03 -0500

    Clean project server

--- models/server.js
+++ models/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const http = require('http')
 const socketio = require('socket.io')
 const path = require('path')
+const cors = require('cors')
 const Sockets = require('./sockets')
 
 class Server {
@@ -15,6 +16,7 @@ class Server {
   }
   middlewares() {
     this.app.use(express.static(path.resolve(__dirname, '../public')))
+    this.app.use(cors())
   }
   configureSockets() {
     new Sockets(this.io)
